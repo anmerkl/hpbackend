26c9a8ffd50efc947f9f59d5d31a5418be808c52
Alfonso Merkl <anmerkl@gmail.com>
2017-12-07 11:20:07 -0500

    added updating and creating models through csv file

--- app/models/system.rb
+++ app/models/system.rb
@@ -1,2 +1,17 @@
 class System < ApplicationRecord
+  require 'csv'
+
+  self.primary_key = :serialNumber
+
+  def self.create_from_csv(csv)
+    CSV.foreach(csv, headers: true) do |row|
+      System.create(row.to_h)
+    end
+  end
+
+  def self.update_from_csv(csv)
+    CSV.foreach(csv, headers: true) do |row|
+      System.update(row.to_h)
+    end
+  end
 end
