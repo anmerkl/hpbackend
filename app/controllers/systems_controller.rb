0a885724674183e2c37821eee4ca4281555163ee
Alfonso Merkl <anmerkl@gmail.com>
2017-12-10 16:31:08 -0500

    took out csv routes. changed to update system through params

--- app/controllers/systems_controller.rb
+++ app/controllers/systems_controller.rb
@@ -3,7 +3,7 @@ class SystemsController < ApplicationController
   def all
     @systems = System.where(companyName: params[:companyName]).to_json
   end
-  
+
   def search
     filtered_systems = System.where(companyName: params[:companyName])
     if params[:systemName]
@@ -34,11 +34,29 @@ class SystemsController < ApplicationController
     @systems = filtered_systems.to_json
   end
 
-  def create
-    System.create_from_csv(params[:csv])
-  end
-
   def update
-    System.update_from_csv(params[:csv])
+    update_system = System.find(params[:serialNumber])
+
+    if params[:systemName]
+      update_system.update(systemName: params[:systemName])
+    end
+    if params[:productFamily]
+      update_system.update(productFamily: params[:productFamily])
+    end
+    if params[:model]
+      update_system.update(model: params[:model])
+    end
+    if params[:osVersion]
+      update_system.update(osVersion: params[:osVersion])
+    end
+    if params[:cpgCount]
+      update_system.update(cpgCount: params[:cpgCount])
+    end
+    if params[:location_region]
+      update_system.update(location_region: params[:location_region])
+    end
+    if params[:location_country]
+      update_system.update(location_country: params[:location_country])
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   get '/systems/:companyName', to: 'systems#all'
   get '/systems/:companyName/search', to: 'systems#search'
-  get '/systems/:companyName/create/:file', to: 'systems#create'
-  get '/systems/:companyName/update/:file', to: 'systems#update'
+  get '/systems/:companyName/update/:serialNumber', to: 'systems#update'
 end
